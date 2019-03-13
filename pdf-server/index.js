854ae1b388e136b338e2566ec733a1ffd78bf3b4
Joseph Thompson <joe.thompson838@gmail.com>
2019-03-13 04:13:44 -0400

    changed port number

--- pdf-server/index.js
+++ pdf-server/index.js
@@ -3,7 +3,7 @@ const express = require("express"),
   cors = require("cors"),
   logger = require("morgan"),
   helmet = require("helmet"),
-  PORT = process.env.PORT || 6500;
+  PORT = process.env.PORT || 5001;
 
 SERVER.use(cors(), logger("dev"), helmet());
 // add a route for pdf creation
